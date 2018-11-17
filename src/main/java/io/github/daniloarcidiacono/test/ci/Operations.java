cc56a55873a860719beb09196996ddfa7213c742
Danilo Arcidiacono <danilo.arcidiacono@gmail.com>
2018-11-17 12:04:00 +0100

    Add empty private constructor to Operations.

--- src/main/java/io/github/daniloarcidiacono/test/ci/Operations.java
+++ src/main/java/io/github/daniloarcidiacono/test/ci/Operations.java
@@ -1,6 +1,11 @@
 package io.github.daniloarcidiacono.test.ci;
 
 public abstract class Operations {
+    // This makes JaCoCo happy (requires version 0.8.0)
+    // https://github.com/jacoco/jacoco/issues/621
+    private Operations() {
+    }
+
     public static int sum(int a, int b) {
         return a + b;
     }
