37262f6133498477844d2be066c09dd38e5ec066
Danilo Arcidiacono <danilo.arcidiacono@gmail.com>
2018-11-17 10:26:57 +0100

    Init Maven project. Add tests.

---
+++ src/main/java/io/github/daniloarcidiacono/test/ci/Operations.java
@@ -0,0 +1,11 @@
+package io.github.daniloarcidiacono.test.ci;
+
+public abstract class Operations {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+}
